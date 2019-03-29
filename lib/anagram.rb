9b1785d546b61c6cec861879237440e60fe8729e
Jenny Chu <jchudesign0930@gmail.com>
2019-03-29 22:57:53 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,32 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :detect
+  
+  
+  def initialize (detector)
+  @detector = detector
+  end
+
+  def simple_matches(detect)
+      detect.each do
+      |i| if i.split("").sort == @detector.split("").sort
+      i
+  end
+  
+  def one_match(listen)
+    listen = self.new
+    listen.match(%w(ab abc bac))
+  end
+  
+  def one_match(matches)
+    matches = self.new
+    matches.match(%w(enlists google inlets banana))
+  end
+  
+  def multiple_anagrams(matches)
+    matches=self.new
+    matches.match(%w(gallery ballerina regally clergy largely leading)).even
+  end    
+  
+  
+end
