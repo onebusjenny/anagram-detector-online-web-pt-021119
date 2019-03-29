9bd0805ba17c9c9dd767ca97ea216df297e9e8dd
Jenny Chu <jchudesign0930@gmail.com>
2019-03-29 22:55:45 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,31 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :detect
+  @all = []
+  
+  def initialize (detect)
+  @detect = detect
+  end
+  
+  def simple_matches(detect)
+    detect = Anagram.new
+    detect.match(%w(hello world zombies pants dipper)) == []
+  end
+  
+  def one_match(listen)
+    listen = self.new
+    listen.match(%w(ab abc bac))
+  end
+  
+  def one_match(matches)
+    matches = self.new
+    matches.match(%w(enlists google inlets banana))
+  end
+  
+  def multiple_anagrams(matches)
+    matches=self.new
+    matches.match(%w(gallery ballerina regally clergy largely leading)).even
+  end    
+  
+  
+end
